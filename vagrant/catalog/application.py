4e9c236ccc6d6a769210d9073b201b9d0595be40
Jordan A Watt <jordanalexwatt@hotmail.com>
2016-01-12 22:43:42 +0000

    Create ORM setup and database schema and html templates

---
+++ vagrant/catalog/application.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/catalog/')
+def index():
+    categories = session.query(Category)
+    return render_template('index.html', categories=categories)
+
+@app.route('/catalog/<int:category_id>/')
+def category(category_id):
+    categories = session.query(Category).filter_by(id = category_id).one()
+    items = session.query(Item).filter_by(category_id = category_id)
+    return render_template('category.html', categories=categories, items=items)
+
+@app.route('/catalog/<int:category_id>/<int:item_id>/')
+def item(category_id, item_id):
+    categories = session.query(Category).filter_by(id = category_id).one()
+    items = session.query(Item).filter_by(id = item_id).one()
+    return render_template('item.html', categories=categories, items=items)
+
+@app.route('/catalog/new/', methods = ['GET', 'POST'])
+def newItem():
+    if request.method == 'POST':
+        newitem = Item(name = request.form['name'], description = request.form['description'], category_id = request.form['category'])
+        session.add(newitem)
+        session.commit()
+        return redirect(url_for('category', category_id = request.form['category']))
+    else:
+        categories = session.query(Category)
+        return render_template('newitem.html', categories = categories)
+
+@app.route('/catalog/<int:category_id>/<int:item_id>/edit/', methods = ['GET', 'POST'])
+def editItem(category_id, item_id):
+    edititem = session.query(Item).filter_by(id = item_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            edititem.name = request.form['name']
+        if request.form['description']:
+            edititem.description = request.form['description']
+        if request.form['category_id']:
+            edititem.category_id = request.form['category_id']
+        session.add(edititem)
+        session.commit()
+        items = session.query(Item).filter_by(id = item_id).one()
+        return redirect(url_for('item', category_id = items.category_id, item_id = items.id))
+    else:
+        categories = session.query(Category)
+        return render_template('edititem.html', categories=categories, items=edititem)
+
+@app.route('/catalog/<int:item_id>/delete/', methods = ['GET', 'POST'])
+def deleteItem(item_id):
+    deleteitem = session.query(Item).filter_by(id = item_id).one()
+    category_id = deleteitem.category_id
+    if request.method == 'POST':
+        session.delete(deleteitem)
+        session.commit()
+        return redirect(url_for('category', category_id = category_id))
+    else:
+        return render_template('deleteitem.html', items=deleteitem)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 8000)
---
+++ vagrant/catalog/catalog_initialize.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+ultimate = Category(name = "Ultimate")
+hockey = Category(name = "Hockey")
+basketball = Category(name = "Basketball")
+snowboarding = Category(name = "Snowboarding")
+soccer = Category(name = "Soccer")
+football = Category(name = "Football")
+golf = Category(name = "Golf")
+lacrosse = Category(name = "Lacrosse")
+baseball = Category(name = "Baseball")
+tennis = Category(name = "Tennis")
+
+
+##session.add_all([ultimate, hockey, basketball, snowboarding, soccer,
+##                 football, golf, lacrosse, baseball, tennis])
+
+session.add_all([
+    Item(name = "Disc", description = "Official 175g game disc!",
+         category = ultimate),
+    Item(name = "Puck", description = "Official hockey puck!",
+         category = hockey),
+    Item(name = "Basketball", description = "Official basketball!",
+         category = basketball),
+    Item(name = "Snowboard", description = "Official snowboard!",
+         category = snowboarding),
+    Item(name = "Soccer Ball", description = "Official soccer ball!",
+         category = soccer),
+    Item(name = "Football", description = "Official football!",
+         category = football),
+    Item(name = "Golf Ball", description = "Official golf ball!",
+         category = golf),
+    Item(name = "Lacrosse Ball", description = "Official lacrosse ball!",
+         category = lacrosse),
+    Item(name = "Baseball", description = "Official baseball ball!",
+         category = baseball),
+    Item(name = "Tennis Ball", description = "Official Tennis ball!",
+         category = tennis)])
+
+session.commit()
